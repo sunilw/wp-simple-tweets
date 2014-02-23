5f9a6c64c77161aa9e7f25d1e1fdef840e6c12c5
Sunil Williams <sunil@sunil.co.nz>
2014-02-23 20:42:39 +1300

    get tweets working

--- swpf-simple-tweets.php
+++ swpf-simple-tweets.php
@@ -10,16 +10,49 @@
  * License:  GPLv2
  */
 
+// Load Twitter class
+require_once('twitteroauth/twitteroauth/twitteroauth.php') ;
 
- 
+//  Define constants
+require_once('simple-tweets-conf.php') ;
+
+// Create the connection
+$twitter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET) ;
+
+// Load the Tweets
+$tweets = $twitter->get('statuses/user_timeline', array('screen_name' => TWITTER_USERNAME, 'exclude_replies' => 'true', 'include_rts' => 'false', 'count' => TWEET_LIMIT));
+
+
+
+if(!empty($tweets)) {
+  foreach($tweets as $tweet) {
+
+
+    $tweetText = $tweet->text ;
+
+    // Make links active
+    $tweetText = preg_replace("/(http:\/\/|(www\.))(([^\s<]{4,68})[^\s<]*)/", '<a href="http:%2f%2f$2$3" target="_blank">$1$2$4</a>', $tweetText) ;
+
+    $foo = "foo" ;
+
+    //  Linkify user mentions
+    
+
+
+    //  Linkify tags
+
+
+    //  Output
+    echo $tweetText;
+
+  }
+}
+
+
+
+
 function produce_tweets(){
-  echo "got here" ;  
-} 
+
+  echo $tweets ;
+}
 add_shortcode( 'publish_tweets', 'produce_tweets' );
-
-
-function my_test(){
-  $mystr =  "<h2>>got here</h2>" ;  
-  return $mystr ;
-} 
-add_shortcode( 'publish_test', 'my_test' );
